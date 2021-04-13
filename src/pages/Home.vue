cf5e998f8a1b2ba81a91c8ca8e790d3c44a971da
unknown <mashagi@yandex.ru>
2021-04-13 14:41:40 +0300

    start page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,44 @@
 <template>
-  <div class="main h-100vh d-block">
+  <div class="main h-100vh d-flex align-items-center">
     <div class="container">
-      <div class="main__content h-100vh py-30 py-md-50 py-lg-70">
-        <div class="pl-lg-100">
-          <p class="pb-80 main__logo">DVCh</p>
-          <h1 class="main__h1"> Da-Vinchi Network. Bringing it all Tohether!</h1>
+      <div class="main__content d-flex py-5">
+        <div class="ml-100 w-35">
+          <router-link to="#" class="link mb-30 link__logo">DVCh</router-link>
+          <h1 class="main__h1 mb-20"> Da-Vinchi Network. Bringing it all Tohether!</h1>
+          <p class="mb-20 main__text">Join us for easy communication and bringing great ideas to the development of client applications</p>
+          <div v-if="onSystem" class="mb-60 d-flex ">
+            <button class="w-50 mr-15 main__btn main__btn--gradient"  @click="onUserPage">
+              My Page
+            </button>
+            <button class="w-50 main__btn"  @click="onLogout">
+              logout
+            </button>
+          </div>
+          <div v-else class="mb-60  d-flex">
+            <button class="mr-15 w-50 main__btn main__btn--gradient" @click="onLogin">
+              Login
+            </button>
+            <button class="main__btn w-50" @click="onReg">
+              Registation
+            </button>
+          </div>
+          <div class="ml-90 mr-60 main__navy">
+            <p class="main__text mb-0">The freshest decisions</p>
+            <h2 class="mb-30 main__h2">Since 2021</h2>
+            <p class="main__text mb-10">Enjoy the team</p>
+            <router-link to="#" class="link link--btnstyle w-100  mb-10">know more</router-link>
+            <p class="main__text">join our collaborator</p>
+          </div>
+        </div>
+        <div class="w-40 ml-30 mt-15">
+          <div class="w-70 img__wrap px-50 py-50  mb-60">
+            <img :src="require('@/assets/img/camera.png')" alt="camera lens picture" class="w-100 img">
+          </div>
+          <div class="d-flex w-70 justify-content-between">
+            <router-link to="#" class="link link__lines lh--14 text--18">Order<br><span class='text--14 main__text'>a project</span></router-link>
+            <router-link to="#" class="link link__lines link__lines--second lh--14 text--18">Сustomer<br><span class='text--14 main__text'>reviews</span></router-link>
+            <router-link to="#" class="link link__lines link__lines--third lh--14 text--18">Abouts<br><span class='text--14 main__text'>us</span></router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -56,32 +90,32 @@
 </template>
 
 <script>
-// export default {
-//   data() {
-//     return {
-//       bgs: ['display_bg-3', 'display_bg-4']
-//     }
-//   },
-//   methods: {
-//     onLogin() {
-//       this.$router.push(`/login`)
-//     },
-//     onReg() {
-//       this.$router.push(`/registration`)
-//     },
-//     onUserPage() {
-//       this.$store.dispatch('fetchUsersDB').then(() => {
-//         this.$router.push(`/current_user`)
-//       })
-//     },
-//     onLogout() {
-//       this.$store.dispatch('logoutUser')
-//     }
-//   },
-//   computed: {
-//     onSystem() {
-//       return this.$store.getters.userLoggedIn
-//     }
-//   }
-// }
+export default {
+  //   data() {
+  //     return {
+  //       bgs: ['display_bg-3', 'display_bg-4']
+  //     }
+  //   },
+  methods: {
+    onLogin() {
+      this.$router.push(`/login`)
+    },
+    onReg() {
+      this.$router.push(`/registration`)
+    },
+    onUserPage() {
+      this.$store.dispatch('fetchUsersDB').then(() => {
+        this.$router.push(`/current_user`)
+      })
+    },
+    onLogout() {
+      this.$store.dispatch('logoutUser')
+    }
+  },
+  computed: {
+    onSystem() {
+      return this.$store.getters.userLoggedIn
+    }
+  }
+}
 </script>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="wrapp">
+  <div class="main h-100vh d-flex align-items-center">
+    <div class="container">
+      <div class="main__content d-flex py-5">
+        <div class="ml-100 w-35">
+          <router-link to="#" class="link mb-100 link__logo">DVCh</router-link>
+          
+          <div class="ml-90 mr-60 main__navy">
+            <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+            
+              <span ref="form" >
+                <input
+                  name="email"
+                  label="Email"
+                  type="email"
+                  v-model="email"
+                  :rules="emailRules"
+                  required
+                >
+               
+                <input
+                  name="password"
+                  label="Password"
+                  type="password"
+                  :rules="passwordRules"
+                  :counter="6"
+                  v-model="password"
+                  required
+                >
+              
+              </span>
+              <button
+                @click="isSubmit"
+                :disabled="!valid"
+                >Login</button
+              >
+          </div>
+          <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+
+          </div>
+        </div>
+        <div class="w-40 ml-30 mt-15">
+          <div class="w-70 img__wrap px-50 py-50  mb-60">
+            <img :src="require('@/assets/img/camera.png')" alt="camera lens picture" class="w-100 img">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- OLD CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+  <!-- <div class="wrapp"> -->
     <!-- <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md6>
@@ -45,53 +95,55 @@
         </v-flex>
       </v-layout>
     </v-container> -->
-  </div>
+  <!-- </div> -->
 </template>
 
 // <script>
-// export default {
-//   data() {
-//     return {
-//       valid: false,
-//       email: '',
-//       password: '',
-//       emailRules: [
-//         v => !!v || 'E-mail is required',
-//         v => /.+@.+/.test(v) || 'E-mail must be valid'
-//       ],
-//       passwordRules: [
-//         v => !!v || 'Password is required',
-//         v =>
-//           v.length >= 6 || 'Password must be equal or than  than 6 characters'
-//       ]
-//     }
-//   },
-//   created() {
-//     if (this.$route.query['loginError']) {
-//       this.$store.dispatch('activeError', 'My friend, try login')
-//       this.$store.dispatch('isErrorColor')
-//       this.$bus.$emit('showModal')
-//     }
-//   },
-//   methods: {
-//     isSubmit() {
-//       if (this.$refs.form.validate()) {
-//         const user = {
-//           email: this.email,
-//           password: this.password
-//         }
+export default {
+  data() {
+    return {
+      // valid: false,
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v =>
+          v.length >= 6 || 'Password must be equal or than  than 6 characters'
+      ]
+    }
+  },
+  // created() {
+  //   if (this.$route.query['loginError']) {
+  //     this.$store.dispatch('activeError', 'My friend, try login')
+  //     this.$store.dispatch('isErrorColor')
+  //     this.$bus.$emit('showModal')
+  //   }
+  // },
+  methods: {
+    isSubmit() {
+      if (this.$refs.form == valid) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
 
-//         this.$store
-//           .dispatch('loginUser', user)
-//           .then(() => {
-//             this.$router.push('/')
-//             this.$bus.$emit('showModal')
-//           })
-//           .catch(() => {
-//             this.$bus.$emit('showModal')
-//           })
-//       }
-//     }
-//   }
-// }
-// </script>
+        this.$store
+          .dispatch('loginUser', user)
+          .then(() => {
+            this.$router.push('/')
+            // this.$bus.$emit('showModal')
+            alert('success')
+          })
+          .catch(() => {
+            // this.$bus.$emit('showModal')
+            alert('fail')
+          })
+      }
+    }
+  }
+}
+</script>
